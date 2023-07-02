acd78570671bb298df77700f0c1e9c6e9ee30eae
Karen/あけみ <karen@akemi.ai>
2023-07-02 23:22:16 +0900

    appinst: Add Dopamine warning message.

--- appinst/appinst.m
+++ appinst/appinst.m
@@ -108,6 +108,13 @@ int main(int argc, const char *argv[]) {
 			printf("Please make sure that you download AppSync Unified from the official repository to ensure proper operation.\n");
 		}
 
+		if (access(ROOT_PATH("/.installed_dopamine"), F_OK) == 0) {
+			printf("WARNING: You appear to be using the Dopamine jailbreak.\n");
+			printf("There is a known IPC (inter-process communication) issue with Dopamine that may prevent apps from successfully installing.\n");
+			printf("It may also cause the TrollStore app to become temporarily unusable.\n");
+			printf("To restore TrollStore functionality, open your persistence helper (such as \"GTA Car Tracker\") and select \"Refresh App Registrations\".\n");
+		}
+
 		// Construct our temporary directory path
 		NSFileManager *fileManager = [NSFileManager defaultManager];
 		NSString *workPath = [NSTemporaryDirectory() stringByAppendingPathComponent:@"appinst"];
